d1e7e5f6cb3050c3c32497420d3d09ee5130d509
Mursaline Huqe <mursalin7842@gmail.com>
2025-08-02 00:38:25 +0600

    update main.py to create an interactive zoo visit story and add new Python_if_else.py file

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-#my first python program
-shopping_item = int(input("enter the number of item :").strip())
-shopping_item +=1
-print(f"here is your total item {shopping_item}")
+adjective1=input(("Enter Your Adjective (Description): "))
+noun1 = input(("Enter your Noun to Visit the zoo (person/place/thing): "))
+adjective2 = input(("Enter your Adjective (Description): "))
+noun2 = input(("Enter your Noun to Visit the zoo (person/place/thing):"))
+adjective3 = input(("Enter your Adjective (Description): "))
+
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exehibit i saw a {noun1} that was {adjective2}")
+print(f"Then I saw a {noun2} that was {adjective3}")
+print(f"Finally I saw a {noun1} that was {adjective2}")
+print(f"Today was a {adjective1} day at the zoo")
