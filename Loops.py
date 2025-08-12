9b4eda28ac9570940139a3269802948e6033c0ee
Mursaline Huqe <mursalin7842@gmail.com>
2025-08-13 02:15:49 +0600

    add division.py for performing integer and float division

--- Loops.py
+++ Loops.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     n = int(input())
     e=0
-while n!=0:
-    print(e**2)
-    e=e+1
-    n=n-1
+if 1<=n<=20:
+    while n!=0:
+        print(e**2)
+        e=e+1
+        n=n-1
