a896347ffcd9d1c975ed894b749ebbbfbe7de2b2
Mursaline Huqe <mursalin7842@gmail.com>
2025-08-13 02:18:02 +0600

    refactor Loops.py to improve input handling and add range validation

--- Loops.py
+++ Loops.py
@@ -1,3 +1,4 @@
+"""
 if __name__ == '__main__':
     n = int(input())
     e=0
@@ -6,3 +7,12 @@ if 1<=n<=20:
         print(e**2)
         e=e+1
         n=n-1
+
+"""
+if __name__ == '__main__':
+    n = int(input())
+    e=0
+if 1<=n<=20:
+    for i in range(0,n):
+        print(e**2)
+        e=e+1
