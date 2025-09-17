a82ffa15c3f891ca89c66b14413635c6812d335d
Mursaline Huqe <mursalin7842@gmail.com>
2025-09-17 22:07:42 +0600

    add coffe.py and house.py for new functionality

---
+++ CoffeHouse/coffe.py
@@ -0,0 +1,9 @@
+from house import house
+
+print("nothing beats  coffee in the morning")
+
+def coffee (n):
+
+    print(f"you are having {n} cups of coffee !! not bad")
+
+house("subedar")
---
+++ CoffeHouse/house.py
@@ -0,0 +1,6 @@
+from coffe import coffee
+
+coffee(2)
+
+def house(n):
+    print(f"you are visiting the {n} house its an amazing place")
