655a8926db8726c512bef7a0ae9785edefdd8946
Mursaline Huqe <mursalin7842@gmail.com>
2025-09-24 20:44:27 +0600

    add functionality to tea.py and new XML migration files

--- CoffeHouse/tea.py
+++ CoffeHouse/tea.py
@@ -1,1 +1,19 @@
-print("Tea is great for relaxation")
+import os
+
+# Current working directory
+print(os.getcwd())
+
+# List files in a directory
+print(os.listdir("."))
+
+# Create a folder
+os.mkdir("test_folder")
+
+# Remove a folder
+os.rmdir("test_folder")
+
+# Environment variables
+print(os.environ.get("PATH"))
+
+# Join paths safely (cross-platform)
+print(os.path.join("folder", "file.txt"))
