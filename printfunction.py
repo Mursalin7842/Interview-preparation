0dfdf8c1d4e9372b5d5f853661a047d6c58383dc
Mursaline Huqe <mursalin7842@gmail.com>
2026-05-09 01:09:58 +0600

    add current working directory output to file_&_directory_operations.py

--- printfunction.py
+++ printfunction.py
@@ -1,6 +1,30 @@
-if __name__ == '__main__':
-    n = int(input())
+#my journey with list [] -> tuple(), list[], set{}, dictionary{'K':'V'} other that qq,ordered dictionary, name tuples
+# no one can beat me
+# I am invincible
+#features - ordered , mutable, anydata types , no fix size, unhashble
 
-    if 1 <= n <= 150:
-        for i in range(1, n + 1):
-            print(i, end="")
+def auto_convertion(value):
+    if value.lower() == 'true':
+        return True
+    if value.lower() == 'false':
+        return False
+
+    if value.isdigit() or value.startswith('-') and value[1:].isdigit():
+        return int(value)
+
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    return value
+busket = []
+i = 0
+while True:
+    user = (input(""))
+    if user == "":
+        break
+    busket.append(auto_convertion(user))
+    print(type(busket[i]))
+    i +=1
+print(busket[1][1:])
